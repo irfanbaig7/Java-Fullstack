a2f8f4f96eb940a13062c6a31211a45873e9974b
irfanbaig7 <ibaig9356@gmail.com>
2025-09-04 15:32:49 +0530

    Deleted src/Phase1/topic1 folder

--- src/Phase1/topic1/helloJava.java
+++
@@ -1,7 +0,0 @@
-package Phase1.topic1;
-
-public class helloJava {
-    public static void main(String[] args) {
-        System.out.println("Hello java Good AfterNoon kaise hai app log");
-    }
-}
