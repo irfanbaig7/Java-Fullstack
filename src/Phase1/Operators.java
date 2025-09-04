681ab5536c4df49347cdb54e470bba72166b9487
irfanbaig7 <ibaig9356@gmail.com>
2025-09-04 16:19:38 +0530

    how to use Opretors, data types and so more things cover in this porsation

--- src/Phase1/Operators.java
+++ src/Phase1/Operators.java
@@ -2,21 +2,39 @@ package Phase1;
 
 public class Operators {
     public static void main(String[] args) {
-        int a = 10, b = 20;
-        System.out.println(a + b);  // 30
-        System.out.println(a > b);  // false
-        System.out.println((a < b) && (b > 15));  // true
+//   Q1: Sum, Difference, Product, Quotient of Two Integers
 
-        // like this we are doing more Oprations like that
+//        int a = 20, b = 10;
+//
+//        System.out.println("Sum: " + (a + b));         // 30
+//        System.out.println("Difference: " + (a - b)); // 10
+//        System.out.println("Product: " + (a * b));    // 200
+//        System.out.println("Quotient: " + (a / b));   // 2
 
-        /*
-        * Arithmetic → + - * / %
-        Relational → == != > < >= <=
-        Logical → && || !
-        Assignment → = += -= *= /=
-        Increment/Decrement → ++ --
-        Ternary → condition ? value1 : value2
-        * */
+
+//        Q2: Check Even or Odd
+
+//        int num = 78;
+//        if (num % 2 == 0) {
+//            System.out.println(num + " is Even");
+//        } else {
+//            System.out.println(num + " is Odd");
+//        }
+
+//        Q3: Swap Two Variables Without Third Variable
+
+//        int a = 5, b = 10;
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+//        System.out.println("After swap : a = " + a + ", b = " + b);
+
+
+//       Q4: Output of Expression
+//
+//        int x = 5;
+//        System.out.println(x++ + ++x);
+
 
     }
 }
