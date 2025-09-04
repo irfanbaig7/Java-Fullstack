aee5945e314956731c23790f952e9ecedb07f933
irfanbaig7 <ibaig9356@gmail.com>
2025-09-04 15:16:04 +0530

    Hello java exacute successfully

--- src/Phase1/topic1/helloJava.java
+++ src/Phase1/topic1/helloJava.java
@@ -2,6 +2,6 @@ package Phase1.topic1;
 
 public class helloJava {
     public static void main(String[] args) {
-        System.out.println("Hello java Good AfterNoon");
+        System.out.println("Hello java Good AfterNoon kaise hai app log");
     }
 }
