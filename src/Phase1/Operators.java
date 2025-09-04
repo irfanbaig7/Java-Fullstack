88cf9f0142e96147f51c19234eed96c5bd5a0625
irfanbaig7 <ibaig9356@gmail.com>
2025-09-04 15:40:14 +0530

    how to use Opretors in java

---
+++ src/Phase1/Operators.java
@@ -0,0 +1,22 @@
+package Phase1;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println(a + b);  // 30
+        System.out.println(a > b);  // false
+        System.out.println((a < b) && (b > 15));  // true
+
+        // like this we are doing more Oprations like that
+
+        /*
+        * Arithmetic → + - * / %
+        Relational → == != > < >= <=
+        Logical → && || !
+        Assignment → = += -= *= /=
+        Increment/Decrement → ++ --
+        Ternary → condition ? value1 : value2
+        * */
+
+    }
+}
