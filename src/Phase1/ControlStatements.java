842361d36e3a511d8be7c7b91d13cccc8742d903
irfanbaig7 <ibaig9356@gmail.com>
2025-09-04 17:22:12 +0530

    in this session we cover topic like decision making, loops, break/continue, switch expressions

---
+++ src/Phase1/ControlStatements.java
@@ -0,0 +1,181 @@
+package Phase1;
+
+public class ControlStatements {
+    public static void main(String[] args) {
+
+//        if-else (Decision Making)
+
+//        int age = 15;
+//        if (age >= 18) {
+//            System.out.println("You can Vote !!");
+//        } else {
+//            System.out.println("You can't Vote !!");
+//        }
+
+//        Nested if-else
+
+//        int marks = 55;
+//        if (marks >= 90) {
+//            System.out.println("Grade A");
+//        } else if (marks >= 75) {
+//            System.out.println("Grade B");
+//        } else {
+//            System.out.println("Grade C");
+//        }
+
+
+//        Switch cases
+
+//        int day = 4;
+//        String result = switch (day) {
+//            case 1 -> "Monday";
+//            case 2 -> "Tuesday";
+//            case 3 -> "Wednesday";
+//            default -> "Invalid";
+//        };
+//        System.out.println(result);
+
+
+//        Loops
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+
+
+//        int i = 1;
+//        while (i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+//        int j = 1;
+//        do {
+//            System.out.println(j);
+//            j++;
+//        } while (j <= 5);
+
+
+//        int[] nums = {10, 20, 30, 50, 60};
+//        for (int n : nums) {
+//            System.out.println(n);
+//        }
+//
+
+
+//        Jump Statement
+
+//        for (int i = 1; i <= 5; i++) {
+//            if (i == 3) continue;   // skip 3
+//            if (i == 5) break;      // stop loop at 5
+//            System.out.println(i);
+//        }
+
+
+//        Qs.  multiplication table
+
+//        int num = 10;
+//        for (int i = 1; i <= num; i++){
+//            System.out.println(num + "x" + i + " = " + (num * i));
+//        }
+        
+        
+        
+//        RverseNumber
+//
+//        int num = 1234, rev = 0;
+//
+//        while (num > 0) {
+//            int digit = num % 10;
+//            rev = rev * 10 + digit;
+//            num /= 10;
+//        }
+//
+//        System.out.println("Reversed: " + rev);
+
+
+//       Q6: Even Numbers 1 to 50
+
+//        for (int i = 1; i <= 50; i++){
+//            if (i % 2 == 0) {
+//                System.out.println(i + " ");
+//            }
+//        }
+
+
+//        Factorial of a Number
+
+//        int n = 5, fact = 1;
+//        while(n > 0){
+//            fact *= n;
+//            n--;
+//        }
+//        System.out.println("factorial = " + fact);
+
+        
+//        check is number prime or not
+        
+//        int num = 29;
+//        boolean isprime = true;
+//
+//        if (num <= 1) {
+//            isprime = false;
+//        }
+//        else {
+//            for (int i = 2; i <= num / 2; i++) {
+//                if (num % i == 0) {
+//                    isprime = false;
+//                    break;
+//                }
+//            }
+//        }
+//
+//
+//        if (isprime) System.out.println("Prime");
+//        else System.out.println("Not Prime");
+
+
+
+//        Fibonacci Series
+//
+//        int n = 7;
+//        int a =0, b=1;
+//
+//        System.out.print(a + " " + b + " ");
+//        for (int i = 3; i <= n; i++){
+//            int c = a + b;
+//            System.out.println(c + " ");
+//            a = b;
+//            b = c;
+//        }
+
+
+//        pridict Tricky Output
+
+//        int i = 0;
+//        while (i++ < 5){
+//            System.out.println(i + " ");
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
